9057fb411dfc48cf64296dc86a10d0cada15a5cc
watzefutsk <pdd.javadeveloper@yandex.ru>
2022-07-08 22:57:11 +0300

    added simple Fibonacci

--- test/src/main/java/Main.java
+++ test/src/main/java/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
+        int[] fibo = new int[8];
+        fibo[0] = 0;
+        fibo[1] = 1;
+
+        for (int i = 2; i < fibo.length; i++) {
+            fibo[i] = fibo[i - 1] + fibo[i - 2];
+        }
+        for (int i = 0; i < fibo.length; i++) {
+            System.out.println(fibo[i]);
+        }
     }
 }
